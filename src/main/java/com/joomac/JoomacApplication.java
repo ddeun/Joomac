09a4b81a9b285eb3cd95d459cd0e028e303e2003
ddeun <tt2279@naver.com>
2026-01-06 11:38:56 +0900

    1월 6일

---
+++ src/main/java/com/joomac/JoomacApplication.java
@@ -0,0 +1,13 @@
+package com.joomac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoomacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoomacApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joomac/JoomacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joomac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoomacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
